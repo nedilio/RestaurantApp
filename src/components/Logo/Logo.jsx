b147a4dcca5a4e027dcf6f0cb21b88b79f86736c
Nelson Izquierdo <izquierdonelson@gmail.com>
2022-06-24 01:01:24 -0400

    fiz: logo importado desde src

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,10 +1,13 @@
 import { Link } from 'react-router-dom';
+import logoUrl from '../../img/logo-example.png';
+import logoUrl2 from '../../img/logoipsum-logo-5.svg';
 import './Logo.css';
+
 const Logo = () => {
   return (
     <div className="teal darken-1 center-align logo-container nav-wrapper">
         <Link to="/">
-            <img src="./img/logo-example.png" alt="logo" className="responsive-img"/>
+            <img src={logoUrl2} alt="logo" className="responsive-img"/>
         </Link>
     </div>
   )
